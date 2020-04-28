bf01b5c54801d1a8287d31382940b380b51d9587
Maxim Gabidullin <mail@maxim-gabidullin.ru>
2020-04-29 00:04:46 +0300

    asdsad

--- js/phone.manipulation.js
+++ js/phone.manipulation.js
@@ -3,8 +3,7 @@ $(function () {
         let phone = this.value;
         // phone = phone.replace(/^\\+7/, '');
         // phone = phone.replace(/^8/, '');
-        phone = phone.replace(/[^0-9]+git push
-        /, '');
+        phone = phone.replace(/[^0-9]/, '');
         $('#phone').val(phone);
     });
 });
